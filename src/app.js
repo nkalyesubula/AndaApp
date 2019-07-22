53d29f2dc06635599b551445c39b5615845588f5
Noah Kalyesubula <noahkalyesubula@Noahs-MacBook-Pro.local>
2019-07-22 16:19:50 +0200

    chore(setup-development-environment): Update setting for API development environment [Finishes #167401602]

--- src/app.js
+++ src/app.js
@@ -2,9 +2,10 @@ import express from "express";
 
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send((message = "hey man"));
+app.get("/", (req, res) => {
+  return res.status(200).send("Welcome to AndaApp");
 });
+
 app.listen((process.env.PORT = 3000), () =>
   console.log("App is listening on port 3000..")
 );
