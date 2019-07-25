a444b35fa4c1758728708824265afb13db22c9a3
jean salvi dukuzwenimana <salvi@jeans-MacBook-Pro-2.local>
2019-07-25 17:53:33 +0200

    ft(adminpart) add admin controller and user service
    [Continue #167188071]

---
+++ src/services/user.js
@@ -0,0 +1,91 @@
+import database from '../src/models';
+
+class UserService {
+  static async getAllUser() {
+    try {
+      return await database.User.findAll();
+    } catch (error) {
+      throw error;
+    }
+  }
+  static async findOneUser(email) {
+    try {
+      return await database.User.findOne({
+        where: { id: String(email)}
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async createUser(newUser) {
+    try {
+      return await database.User.create(newUser);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async updateUser(id, updateUser) {
+    try {
+      const UserToUpdate = await database.User.findOne({
+        where: { id: Number(id) }
+      });
+
+      if (UserToUpdate) {
+        await database.User.update(updateUser, { where: { id: Number(id) } });
+
+        return updateUser;
+      }
+      return null;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async getOneUser(id) {
+    try {
+      const theUser = await database.User.findOne({
+        where: { id: Number(id) }
+      });
+
+      return theUser;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async deleteUser(id) {
+    try {
+      const UserToDelete = await database.User.findOne({ where: { id: Number(id) } });
+
+      if (UserToDelete) {
+        const deletedUser = await database.User.destroy({
+          where: { id: Number(id) }
+        });
+        return deletedUser;
+      }
+      return null;
+    } catch (error) {
+      throw error;
+    }
+  }
+  static async getUserByCol(col) {
+      let column = col.toString()
+    try {
+      const UserToDelete = await database.User.findAll({ where: { this: String(col) } });
+
+      if (UserToDelete) {
+        const deletedUser = await database.User.destroy({
+          where: { id: Number(id) }
+        });
+        return deletedUser;
+      }
+      return null;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+export default UserService;
