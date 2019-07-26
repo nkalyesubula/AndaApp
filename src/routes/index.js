e2ebc28b891f9a4accdfb9fc011da8e022ef0ab2
nshutijonathan <nshutijonathan130@gmail.com>
2019-07-26 10:14:34 +0200

    [start:#167188025]rebasing develop

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import usersRoutes from "../routes/users";
 import articlesRoutes from "../routes/articles";
+import blogsRoutes from '../routes/blogs';
 export default app => {
   app.use(usersRoutes);
   app.use(articlesRoutes);
+  app.use(blogsRoutes);
 };
