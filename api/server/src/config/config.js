251260cc1127d0f103ad9fda4f34177683df5317
nkalyesubula <noahkalyesubula@gmail.com>
2020-03-13 18:48:58 +0200

    setup project

---
+++ api/server/src/config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres"
+  },
+  test: {
+    username: 'noahkalyesubula',
+    password: null,
+    database: 'andapp_test',
+    host: process.env.DB_HOST,
+    dialect: "postgres"
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    use_env_variable: process.env.DATABASE_URL,
+    dialect: 'postgres'
+  }
+};
