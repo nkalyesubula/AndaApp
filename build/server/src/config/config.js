30b0ea64c7674f3d02d07ca64f9034e35a74e525
nkalyesubula <noahkalyesubula@gmail.com>
2020-03-13 18:51:21 +0200

    setup project

---
+++ build/server/src/config/config.js
@@ -0,0 +1,29 @@
+"use strict";
+
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres"
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: null,
+    database: process.env.DB_NAME_TEST,
+    host: process.env.DB_HOST,
+    dialect: "postgres"
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    use_env_variable: process.env.DATABASE_URL,
+    dialect: 'postgres'
+  }
+};
+//# sourceMappingURL=config.js.map
